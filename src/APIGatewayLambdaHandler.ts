26f3eee4090ab7523c15faeefb23689b4b2189c4
Tim Klein <timjklein36@gmail.com>
2018-06-27 18:23:33 -0400

    Genericized APIGatewayLambdaHandler generator

--- src/APIGatewayLambdaHandler.ts
+++ src/APIGatewayLambdaHandler.ts
@@ -1,17 +1,18 @@
 import {
-    APIGatewayProxyCallback,
     APIGatewayProxyEvent,
-    APIGatewayProxyHandler,
+    Callback,
     Context,
+    CustomAuthorizerEvent,
+    Handler,
 } from 'aws-lambda';
 import { GenericLambda } from './GenericLambda';
 
 export class APIGatewayLambdaHandler {
-    public static generate<T extends GenericLambda>(type: { new (...args: any[]): T }) {
-        const handler: APIGatewayProxyHandler = (
-            event: APIGatewayProxyEvent,
+    static generate<T extends GenericLambda>(type: { new (...args: any[]): T }) {
+        const handler: Handler = (
+            event: APIGatewayProxyEvent | CustomAuthorizerEvent,
             context: Context,
-            callback: APIGatewayProxyCallback,
+            callback: Callback,
         ) => {
             new type(event, context, callback).handler();
         };

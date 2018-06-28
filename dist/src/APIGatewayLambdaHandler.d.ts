5d7ecd4d625524f3053d0e8ae1310d5e6a36a185
Tim Klein <timjklein36@gmail.com>
2018-06-27 20:25:42 -0400

    Remove extra old files

--- dist/src/APIGatewayLambdaHandler.d.ts
+++
@@ -1,7 +0,0 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { GenericLambda } from './GenericLambda';
-export declare class APIGatewayLambdaHandler {
-    static generate<T extends GenericLambda>(type: {
-        new (...args: any[]): T;
-    }): import("../node_modules/@types/aws-lambda/index").Handler<APIGatewayProxyEvent, import("../node_modules/@types/aws-lambda/index").APIGatewayProxyResult>;
-}

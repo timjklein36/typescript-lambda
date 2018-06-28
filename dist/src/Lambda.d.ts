7447999adb3bb8bc5cb0189049f13729d3ef1476
Tim Klein <timjklein36@gmail.com>
2018-06-27 20:32:36 -0400

    Rebuild

--- dist/src/Lambda.d.ts
+++ dist/src/Lambda.d.ts
@@ -1,9 +1,9 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Callback, Context } from 'aws-lambda';
+import { Callback, Context } from 'aws-lambda';
 import { LambdaOptions } from './LambdaOptions';
 export declare function Lambda(options: LambdaOptions): <T extends new (...args: any[]) => {}>(target: T) => {
     new (...args: any[]): {
-        event: APIGatewayProxyEvent;
+        event: any;
         context: Context;
-        callback: Callback<APIGatewayProxyResult>;
+        callback: Callback<any>;
     };
 } & T;
